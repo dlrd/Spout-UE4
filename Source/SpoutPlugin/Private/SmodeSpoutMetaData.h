4a0a64b586aa344ce5098be65da014bc67492b02
Qlex42 <alexandre.buge@gmail.com>
2020-10-11 15:29:07 +0200

    * new: dual float4x4 matrix metadata Spout exchange (dlrd/Smode-Issues#4774)

--- Source/SpoutPlugin/Private/SmodeSpoutMetaData.h
+++ Source/SpoutPlugin/Private/SmodeSpoutMetaData.h
@@ -26,6 +26,8 @@ struct SmodeSpoutMetaData
   wchar_t smodeHeader[6];
   std::chrono::high_resolution_clock::time_point frameTime;
   size_t frameNumber = ~0ULL;
+  float matrix1[16] = { 0, };
+  float matrix2[16] = { 0, };
 
   bool isValid() const
     {return memcmp(smodeHeader, L"Smode", sizeof(smodeHeader)) == 0;}
@@ -34,8 +36,12 @@ struct SmodeSpoutMetaData
   {
     frameNumber = ~0ULL;
     frameTime = {};
+    memset(matrix1, 0, sizeof(matrix1));
+    memset(matrix2, 0, sizeof(matrix2));
   }
 };
+
+static_assert(sizeof(SmodeSpoutMetaData) < 256, "SmodeSpoutMetaData structure cannot be greater than Spout description field");
 
 #ifdef WIN32
 #	pragma pack(pop)
